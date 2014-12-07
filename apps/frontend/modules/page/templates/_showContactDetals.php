e67a2a982f6e2917a85eef2626a8b05b3369c9a7
parzygnat <parzygnat@d8802259-b0e8-48e8-9e64-ea36b2df42f5>
2014-12-07 18:40:08 +0000

    #MOD Telefon w kontakcie #FIX tlumaczenia w PL
    git-svn-id: https://madebyrogal.svn.cloudforge.com/napromed@24 d8802259-b0e8-48e8-9e64-ea36b2df42f5

--- apps/frontend/modules/page/templates/_showContactDetals.php
+++ apps/frontend/modules/page/templates/_showContactDetals.php
@@ -1,4 +1,5 @@
 <div class="about">
-    <a title="Dane kontaktowe" href="mailto:<?= $mailData->getEmailFrom() ?>"><?= $mailData->getEmailFrom() ?></a>
+    <a title="Dane kontaktowe" href="mailto:<?= $mailData->getEmailFrom() ?>"><?= $mailData->getEmailFrom() ?></a><br/>
+    +48 5359 - NAPRO<br/>
     <a class="telephon" title="<?= $mailData->getPhone() ?>" href="tel:<?= $mailData->getPhone() ?>"><?= $mailData->getPhone() ?></a>
 </div>
