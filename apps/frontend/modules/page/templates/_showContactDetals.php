39b0a2ce16f809edc0f6908b982e802c66818564
asiula <asiula@d8802259-b0e8-48e8-9e64-ea36b2df42f5>
2014-12-17 22:01:59 +0000

    poprawienie telefonu w menu bocznym
    git-svn-id: https://madebyrogal.svn.cloudforge.com/napromed@28 d8802259-b0e8-48e8-9e64-ea36b2df42f5

--- apps/frontend/modules/page/templates/_showContactDetals.php
+++ apps/frontend/modules/page/templates/_showContactDetals.php
@@ -1,5 +1,5 @@
 <div class="about">
-    <a title="Dane kontaktowe" href="mailto:<?= $mailData->getEmailFrom() ?>"><?= $mailData->getEmailFrom() ?></a><br/>
-    +48 5359 - NAPRO<br/>
+    <a title="Dane kontaktowe" href="mailto:<?= $mailData->getEmailFrom() ?>"><?= $mailData->getEmailFrom() ?></a>
+    <a class="telephon" title="+48 5359 - NAPRO" href="tel:<?= $mailData->getPhone() ?>">+48 5359 - NAPRO</a>
     <a class="telephon" title="<?= $mailData->getPhone() ?>" href="tel:<?= $mailData->getPhone() ?>"><?= $mailData->getPhone() ?></a>
 </div>
