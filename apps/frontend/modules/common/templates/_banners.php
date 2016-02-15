cfeb413d5f411df8c568f3efda923897ceac1e76
madebyrogal <tomasz@madebyrogal.com>
2016-02-15 20:26:28 +0100

    #MOD Banners

---
+++ apps/frontend/modules/common/templates/_banners.php
@@ -0,0 +1,9 @@
+<div style="text-align: center">
+    <a target = "_blank" href = "http://www.fertilman.pl"><img src = "/images/banners/FertilManPlus.jpg"></a><br/>
+    <a target = "_blank" href = " https://www.pola-app.pl/">
+        <img src = "/images/banners/POLA.png" width="200"><br/>
+        <b>Kupuj polskie</b>
+    </a>
+</div>
+
+
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -28,8 +28,7 @@
             <aside>
                 <?php include_component('page', 'showMenuHeader') ?>
                 <?php include_component('page', 'showContactDetals') ?>
-                <a target="_blank" href="http://www.wydawnictwofronda.pl/nadzieja-na-dziecko-czyli-cala-prawda-o-naprotechnologii-3"><img src="/images/nadzieja.jpg"></a>
-                <a href="http://napromed.pl/naprotechnologia-do-pobrania.html"><img src="/images/in-vitro.jpg"></a>
+                <?php include_partial('common/banners'); ?>                
             </aside>
             <section class="content">
                 <?= $sf_content ?>                
